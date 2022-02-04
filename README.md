59caf8a3cb407de3f3d17e69729256924bca77fb
Nareshkumar Rao <contact@nareshkumarrao.com>
2022-02-04 02:50:57 +0100

    updated readme, install-vim-defaults

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 ## Vim
 
+Vim one-click setup:
+```bash
+curl -s https://raw.githubusercontent.com/naresh97/defaults-templates/main/vim/install-vim-defaults.sh | bash -s
+```
--- vim/install-vim-defaults.sh
+++ vim/install-vim-defaults.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt update && apt install -y -q vim
+apt update && apt install -y -q vim git curl
 curl -s https://raw.githubusercontent.com/naresh97/defaults-templates/main/vim/install-plugins.sh | bash -s
 curl -s https://raw.githubusercontent.com/naresh97/defaults-templates/main/vim/vimrc > ~/.vimrc
 
